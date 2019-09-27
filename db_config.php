97347d38011295e6755df89e9676134786a0c0e6
Miro <50032840+ml4ch@users.noreply.github.com>
2019-09-27 20:47:10 +0200

    Source code for the server (database API-script)

--- db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define('MYSQL_HOST','127.0.0.1');
-	define('MYSQL_USER','enter_username');
-	define('MYSQL_PASS','enter_password');
-	define('MYSQL_DB1','CoSESWeather_DB');
-	define('MYSQL_DB2','WeeWx_DB');	
-?>
